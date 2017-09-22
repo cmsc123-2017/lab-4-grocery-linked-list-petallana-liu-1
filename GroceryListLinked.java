68170c70da3352477d2e5a76983b3391b10536cf
Jeane Petallana <mhpetallana@up.edu.ph>
2017-09-23 01:03:15 +0800

    To be modified LinkedList

--- GroceryListLinked.java
+++ GroceryListLinked.java
@@ -6,19 +6,56 @@ class GroceryListLinked implements IGroceryList {
   GroceryListLinked() {}
   
   public boolean add(GroceryItem item) {
-    return false;
+ 
+    GroceryNode newNode = new GroceryNode(item, head);
+    head = newNode;
+    this.size++;
+   
+    return true;
   }
   
   public boolean remove(String name) {
-    return false;
+    GroceryItem item = new GroceryItem(name, 0);
+    GroceryNode current = head; //store head
+    GroceryNode before = null;
+    boolean wasDeleted = false;
+ 
+    if (current != null && current.data == item){ //if head contains name to be deleted
+      head = current.next; //change head
+    }
+   
+    while (current != null && current.data != item){ //look for match
+        before = current;
+        current = current.next;
+       
+        this.size--;
+    }
+   
+    if (current == null) { //if not present wasDeleted is now false
+      wasDeleted = false;   
+    } else {
+      wasDeleted = true; //if present was Deleted is now true
+    }
+   
+    before.next = current.next; //unlink node from list
+    return wasDeleted;  //return was Deleted
   }
   
   public boolean markAsBought(String name) {
+    GroceryNode current = head;
+    GroceryNode before = current;
+    
+    if (current.data == new GroceryItem(name, 0)) {
+      current = current.next.next;
+      return true;
+    }
     return false;
   }
   
   public void display() {
-    
+    for (int i = 0; i < this.size; i++) {
+      System.out.println();
+    }
   }
 }
 
